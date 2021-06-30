5eb534d4477bb1212e3245723d92970a3c56dd88
Toni Paternina <git@tonipaternina.com>
2021-06-30 19:45:50 +0200

    start adding functionality to currency change button

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -1,6 +1,40 @@
-import { StyledCurrency } from "./Currency.styles"
+import React from "react";
+import { DownCircleOutlined } from "@ant-design/icons";
+import { StyledCurrency, StyledDollar, StyledInput } from "./Currency.styles";
 
-export default function Currency(props) {
-  // TODO: implement currency selection functionality
-  return (<StyledCurrency>{props.currency}</StyledCurrency>)
-} 
+export default class Currency extends React.Component {
+  state = {
+    currency: this.props.currency,
+    searchTerm: "",
+    currencyList: [],
+    isActive: false,
+  };
+
+  handleClick = (e) => {
+    console.log("click!");
+    this.setState({ isActive: !this.state.isActive });
+    // GET AND DISPLAY LIST OF COINS
+  };
+
+  handleChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    this.setState({ searchTerm: value });
+  };
+
+  render() {
+    return (
+      <StyledCurrency>
+        <StyledDollar />
+        {!this.state.isActive && <span>{this.state.currency}</span>}
+        {this.state.isActive && (
+          <form>
+            <StyledInput value={this.state.currency} />
+          </form>
+        )}
+        <DownCircleOutlined onClick={this.handleClick} />
+      </StyledCurrency>
+    );
+  }
+}
--- src/components/Currency/Currency.styles.js
+++ src/components/Currency/Currency.styles.js
@@ -1,13 +1,28 @@
 import styled from "styled-components";
+import { DollarCircleOutlined } from "@ant-design/icons"
 
 export const StyledCurrency = styled.button`
   background-color: #2c2f36;
   border-radius: 15px;
-  padding: 0.5rem 1.5rem;
+  padding: 0.5rem 1rem;
   font-size: 1rem;
   color: #fff;
   border: none;
   height: fit-content;
   text-transform: uppercase;
-
+  display: flex;
+  justify-content: space-between;
 `;
+
+export const StyledDollar = styled(DollarCircleOutlined)`
+  margin-right: 0.5rem;
+`;
+
+export const StyledInput = styled.input`
+  background-color: #2c2f36;
+  border: none;
+  width: min-content;
+  margin: 0;
+  padding: 0;
+
+`
